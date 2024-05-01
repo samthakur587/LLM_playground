1a0e2cf5662b86a4d3ad48d6eb67c6cc473387e0
Kacper-W-Kozdon <k.kozdon@gmail.com>
2024-05-01 14:09:53 +0200

    leaderboards overhauled; gsheets connection

--- chatbot_arena.py
+++ chatbot_arena.py
@@ -55,17 +55,14 @@ with open("detail_leaderboards.json", "r") as in_file:
     st.session_state.detailed_leaderboards = json.load(in_file)
 
 
-conn = st.connection("gsheets", type=GSheetsConnection)
+conn = st.connection("gsheets", type=GSheetsConnection, url="https://docs.google.com/spreadsheets/d/10QrEik70RYY_LM8RW8GGq-vZWK2e1dka6agRGtKZPHU/edit?usp=sharing")
 
 
-gsheets_leaderboard = conn.read(url="https://docs.google.com/spreadsheets/d/10QrEik70RYY_LM8RW8GGq-vZWK2e1dka6agRGtKZPHU/edit?usp=sharing",
-                                spreadsheet="leaderboard",
+gsheets_leaderboard = conn.read(spreadsheet="leaderboard",
                                 worksheet="leaderboard")
-gsheets_detail_leaderboard = conn.read(url="https://docs.google.com/spreadsheets/d/10QrEik70RYY_LM8RW8GGq-vZWK2e1dka6agRGtKZPHU/edit?usp=sharing",
-                                       spreadsheet="leaderboard",
+gsheets_detail_leaderboard = conn.read(spreadsheet="leaderboard",
                                        worksheet="detail_leaderboard")
-gsheet_models = conn.read(url="https://docs.google.com/spreadsheets/d/10QrEik70RYY_LM8RW8GGq-vZWK2e1dka6agRGtKZPHU/edit?usp=sharing",
-                          spreadsheet="leaderboard",
+gsheet_models = conn.read(spreadsheet="leaderboard",
                           worksheet="models")
 
 
