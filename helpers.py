7786f1695006789d4e4650432a958a87af909cda
Kacper-W-Kozdon <k.kozdon@gmail.com>
2024-05-03 13:10:29 +0200

    online update

--- helpers.py
+++ helpers.py
@@ -27,6 +27,10 @@ class database:
         with open("models.json", "r") as f:
             data = json.load(f)
 
+        if not os.path.exists("./models.csv"):
+            models_df = pd.DataFrame(data)
+            models_df.to_csv("models.csv")
+
         all_models = tuple(data['models'])
 
         if not os.path.exists("./leaderboard.csv"):
@@ -101,6 +105,23 @@ class database:
         sorted_counts_df.to_csv('leaderboard.csv', index=False)
 
     def save_online():
+
+        keys = ["leaderboard", "detail", "models"]
+        for key in keys:
+            if key not in st.session_state.keys():
+                st.session_state[key] = None
+
+        sorted_counts = sorted(st.session_state['vote_counts'].items(), key=lambda x: x[1]["Wins ⭐"] + x[1]["Losses ❌"], reverse=True)
+        for idx, votes in enumerate(sorted_counts):
+            sorted_counts[idx] = (votes[0], votes[1]["Wins ⭐"], votes[1]["Losses ❌"])
+
+        sorted_counts_df = pd.DataFrame(sorted_counts, columns=['Model Name', 'Wins ⭐', 'Losses ❌'])
+
+        detail_leaderboards = st.session_state.detailed_leaderboards
+
+        models = st.session_state.models
+        
+
         # with st.echo():
         #     # Create GSheets connection
         #     conn = st.connection("gsheets", type=GSheetsConnection)
